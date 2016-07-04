5f33ac22d7b13cb012265041b58df09af45be607
PrakharGarg <prakhargarg@utexas.edu>
2016-07-04 17:20:09 -0400

    checking out chartkick

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,12 @@ class HomeController < ApplicationController
   
   def index
     
+    @array = []
+    Weight.find_each do |entry|
+      if entry.user_id == current_user.id
+        @array.push(:created_at => entry.created_at, :weight => entry.weight)
+      end
+    end
+    
   end
 end
