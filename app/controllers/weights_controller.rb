c1f1b390adeff3e150a549e33680a62c87ef2216
PrakharGarg <prakhargarg@utexas.edu>
2016-12-16 09:48:48 -0600

    done

--- app/controllers/weights_controller.rb
+++ app/controllers/weights_controller.rb
@@ -11,7 +11,13 @@ class WeightsController < ApplicationController
     @weight = Weight.new(weight: params[:new_weight], user_id: params[:user_id])
     
     if @weight.save
-         redirect_to root_path, alert: "Successfully updated weight."
+      binding.pry
+      latest_weight_array = Weight.where("user_id = #{current_user.id}")
+      latest_weight = latest_weight_array.order("created_at")[-2]
+      if latest_weight.created_at >= Time.zone.now.beginning_of_day
+        latest_weight.destroy
+      end
+      redirect_to root_path, alert: "Successfully updated weight."
     else
         redirect_to root_path, alert: "Error updating weight. Please try again."
     end
